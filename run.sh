202df968d1e9dbdc141342928b9a8a74fc26e867
Doru Dumitru <doru.g.dumitru@gmail.com>
2025-11-05 12:03:08 +0200

    separated personal and work scripts

--- run.sh
+++ run.sh
@@ -1,11 +1,33 @@
 #!/usr/bin/env bash
 
-script_dir=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
+current_dir=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 filter=""
 dry="0"
+work="0"
 
-cd "$script_dir" || exit
-scripts=$(find ./scripts -maxdepth 1 -mindepth 1 -executable -type f)
+cd "$current_dir" || exit
+personal_scripts=$(find ./scripts/personal -maxdepth 1 -mindepth 1 -executable -type f)
+work_scripts=$(find ./scripts/work -maxdepth 1 -mindepth 1 -executable -type f)
+
+usage() {
+  cat <<EOF
+
+Usage: ${0} [OPTIONS] [FILTER]
+
+Runs scripts found in the './scripts/personal' directory.
+
+Arguments:
+  [FILTER]      An optional string used to filter which scripts to run.
+                Only scripts whose names contain this string will be executed.
+
+Options:
+  --dry         Sets the dry run flag. Scripts will log their intended actions
+                but will not actually execute any commands.
+  --work        Include scripts found in the './scripts/work' directory.
+  -h, --help    Display this help message and exit.
+
+EOF
+}
 
 log() {
   if [[ $dry == "1" ]]; then
@@ -27,6 +49,11 @@ execute() {
 while [[ $# -gt 0 ]]; do
   if [[ $1 == "--dry" ]]; then
     dry="1"
+  elif [[ $1 == "--work" ]]; then
+    work="1"
+  elif [[ $1 == "-h" ]] || [[ $1 == "--help" ]]; then
+    usage
+    exit 0
   else
     filter=$1
   fi
@@ -34,24 +61,35 @@ while [[ $# -gt 0 ]]; do
   shift
 done
 
-LAST_SCRIPTS="utilities.sh dotfiles.sh"
-deferred_scripts=()
+if [[ $work == "1" ]]; then
+  for script in $work_scripts; do
+    if echo "$script" | grep -qv "$filter"; then
+      continue
+    fi
 
-for script in $scripts; do
-  if echo "$script" | grep -qv "$filter"; then
-    continue
-  fi
+    script_name=$(basename "$script")
+    execute "$script"
+  done
+else
+  LAST_SCRIPTS="utilities.sh dotfiles.sh"
+  deferred_scripts=()
 
-  script_name=$(basename "$script")
+  for script in $personal_scripts; do
+    if echo "$script" | grep -qv "$filter"; then
+      continue
+    fi
 
-  if [[ "$LAST_SCRIPTS" =~ $script_name ]]; then
-    deferred_scripts+=("$script")
-    continue
-  fi
+    script_name=$(basename "$script")
 
-  execute "$script"
-done
+    if [[ "$LAST_SCRIPTS" =~ $script_name ]]; then
+      deferred_scripts+=("$script")
+      continue
+    fi
 
-for script in "${deferred_scripts[@]}"; do
-  execute "$script"
-done
+    execute "$script"
+  done
+
+  for script in "${deferred_scripts[@]}"; do
+    execute "$script"
+  done
+fi
