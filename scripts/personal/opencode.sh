328a56ccbbef7c13e2b9ec546f79f5f59c48aba8
Doru Dumitru <doru.dumitru@hartehanks.com>
2026-02-19 10:57:37 +0200

    updates

--- scripts/personal/opencode.sh
+++ scripts/personal/opencode.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+cp ~/.devenv/dotfiles/zsh/.zshrc ~/.devenv/dotfiles/zsh/.zshrc.backup
 curl -fsSL https://opencode.ai/install | bash
+rm ~/.devenv/dotfiles/zsh/.zshrc
+mv ~/.devenv/dotfiles/zsh/.zshrc.backup ~/.devenv/dotfiles/zsh/.zshrc
 
 echo -e "\nOpenCode installed successfully!"
--- scripts/personal/pnpm.sh
+++ scripts/personal/pnpm.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+cp ~/.devenv/dotfiles/zsh/.zshrc ~/.devenv/dotfiles/zsh/.zshrc.backup
 curl -fsSL https://get.pnpm.io/install.sh | sh -
+rm ~/.devenv/dotfiles/zsh/.zshrc
+mv ~/.devenv/dotfiles/zsh/.zshrc.backup ~/.devenv/dotfiles/zsh/.zshrc
 
 echo -e "\npnpm installed successfully!"
