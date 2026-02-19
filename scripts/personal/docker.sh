4751ab23f4ac9fb8a43837e5d05e0bbc2495e52f
Doru Dumitru <doru.g.dumitru@gmail.com>
2026-02-19 15:55:26 +0200

    updates

---
+++ scripts/personal/docker.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+sudo dnf remove -y \
+  docker \
+  docker-client \
+  docker-client-latest \
+  docker-common \
+  docker-latest \
+  docker-latest-logrotate \
+  docker-logrotate \
+  docker-engine || true
+
+sudo dnf -y install dnf-plugins-core
+sudo dnf config-manager --add-repo https://download.docker.com/linux/fedora/docker-ce.repo
+sudo dnf install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo systemctl enable --now docker
+sudo groupadd docker
+sudo usermod -aG docker "$USER"
+
+echo -e "\nDocker installed successfully! You must log out and back in for group changes to take effect."
