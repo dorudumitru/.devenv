c5cf03fedcc8b0ce9b86eca8561894c40a43e870
Doru Dumitru <doru.dumitru@hartehanks.com>
2026-02-19 16:06:39 +0200

    changes

--- scripts/personal/docker.sh
+++ scripts/personal/docker.sh
@@ -11,7 +11,7 @@ sudo dnf remove -y \
   docker-engine || true
 
 sudo dnf -y install dnf-plugins-core
-sudo dnf config-manager --add-repo https://download.docker.com/linux/fedora/docker-ce.repo
+sudo dnf config-manager addrepo --from-repofile https://download.docker.com/linux/fedora/docker-ce.repo
 sudo dnf install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 sudo systemctl enable --now docker
 sudo groupadd docker
