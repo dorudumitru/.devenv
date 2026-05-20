b345747cc29e98d265bc59d5af841609cdf65770
Doru Dumitru <doru.g.dumitru@gmail.com>
2026-05-20 10:38:02 +0300

    updates

--- scripts/personal/fonts.sh
+++ scripts/personal/fonts.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-yay -S --needed --noconfirm inter-font ttf-jetbrains-mono ttf-nerd-fonts-symbols gnome-characters
+yay -S --needed --noconfirm inter-font ttf-jetbrains-mono ttf-nerd-fonts-symbols gnome-characters ttf-lilex ttf-lilex-variable
 
 echo -e "\nFonts installed successfully!"
