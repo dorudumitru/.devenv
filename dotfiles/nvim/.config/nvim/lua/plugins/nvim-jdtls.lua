1724bdf3ad79f6b0124df73cbe2e415ec60ee359
Doru Dumitru <doru.g.dumitru@gmail.com>
2026-06-04 20:02:40 +0300

    updates

--- dotfiles/nvim/.config/nvim/lua/plugins/nvim-jdtls.lua
+++ dotfiles/nvim/.config/nvim/lua/plugins/nvim-jdtls.lua
@@ -1,16 +1,22 @@
 return {
   "mfussenegger/nvim-jdtls",
   opts = function(_, opts)
-    opts.settings.java.inlayHints = {
-      parameterNames = { enabled = "none" },
-    }
+    opts.settings = opts.settings or {}
+    opts.settings.java = opts.settings.java or {}
+
+    -- opts.settings.java.inlayHints = {
+    --   parameterNames = { enabled = "none" },
+    -- }
+
     opts.jdtls = function(config)
       if LazyVim.has("spring-boot.nvim") then
         config.init_options = config.init_options or {}
         config.init_options.bundles =
           vim.list_extend(vim.deepcopy(config.init_options.bundles or {}), require("spring_boot").java_extensions())
       end
+      return config
     end
+
     return opts
   end,
 }
